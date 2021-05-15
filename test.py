d25aa257b62673b8667fa6226aa3b5495c487efa
teaho0311 <55720848+hellowarts@users.noreply.github.com>
2021-05-15 14:28:50 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,17 +1,7 @@
-n = int(input())
 nums = list(map(int, input().split()))
-
-count = [0]*1000000
-
-for i in nums:
-    try: count[i] += 1
-    except: count[i]=1
-
 nums.sort()
 
-befor = -1
-
-print("3:"+str(count[3]))
-print("13:"+str(count[13]))
-print("31:"+str(count[31]))
-print("303:"+str(count[303]))
+if(nums[0] + nums[1] > nums[2]):
+    print("Yes")
+else:
+    print("No")
